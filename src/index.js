c31fc7162bdd265c7555db18c3c2448849898eee
raibaebe <kasimkizyraikhan@gmail.com>
2025-02-16 11:17:42 +0500

    d

--- src/index.js
+++ src/index.js
@@ -11,10 +11,16 @@ connectDB();
 // Middleware
 app.use(express.json());
 
+// Root route to prevent "Cannot GET /" error
+app.get('/', (req, res) => {
+  res.send('API is running...');
+});
+
 // Routes
 app.use('/api', require('./routes/productRoutes'));
 app.use('/api', require('./routes/userRoutes'));
 app.use('/api', require('./routes/orderRoutes'));
+
 // Start server
 app.listen(port, () => {
   console.log(`Server running on port ${port}`);
